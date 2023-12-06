9a34ff6345dd9faf3e9b1be1899cd482cf877dda
Slime5 <akarius12@gmail.com>
2023-12-06 13:09:32 -0500

    adding outline for sign in page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBLKAChIxXjfnJv2pF0j7odVf96qVVkqL8",
-  authDomain: "cscia225.firebaseapp.com",
-  projectId: "cscia225",
-  storageBucket: "cscia225.appspot.com",
-  messagingSenderId: "122711494384",
-  appId: "1:122711494384:web:0a695ba2da3a5b669bbc35",
+    apiKey: "AIzaSyBLKAChIxXjfnJv2pF0j7odVf96qVVkqL8",
+    authDomain: "cscia225.firebaseapp.com",
+    projectId: "cscia225",
+    storageBucket: "cscia225.appspot.com",
+    messagingSenderId: "122711494384",
+    appId: "1:122711494384:web:0a695ba2da3a5b669bbc35"
 };
 firebase.initializeApp(firebaseConfig);
